a33eb032fce83dc296747a36827fa53f2f5d7d4f
m4v8r7s <79208623+m4v8r7s@users.noreply.github.com>
2023-04-16 09:52:54 +0530

    added background video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,10 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udit_portfolio';
-  items!: MenuItem[];
-
-
+  
 
   ngOnInit() {
-    this.items = [
-      { label: 'Home', icon: 'pi pi-fw pi-home' },
-      { label: 'About us', icon: 'pi pi-info-circle' },
-      { label: 'Resume', icon: 'pi pi-file-o' },
-      { label: 'Skills', icon: 'pi pi-palette' },
-      { label: 'Projects', icon: 'pi pi-github' },
-      { label: 'My Blog', icon:'pi pi-book'},
-      { label: 'Contact', icon:'pi pi-phone'}
-    ];
+   
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { VideoBackgroundComponent } from './video-background/video-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoBackgroundComponent
   ],
   imports: [
     BrowserModule,
