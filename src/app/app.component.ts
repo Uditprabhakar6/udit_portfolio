f33147136b291f7b4400b4403b536bf40b93362f
m4v8r7s <79208623+m4v8r7s@users.noreply.github.com>
2023-04-16 08:25:26 +0530

    added nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'udit_portfolio';
+  items!: MenuItem[];
+
+
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home' },
+      { label: 'About us', icon: 'pi pi-info-circle' },
+      { label: 'Resume', icon: 'pi pi-file-o' },
+      { label: 'Skills', icon: 'pi pi-palette' },
+      { label: 'Projects', icon: 'pi pi-github' },
+      { label: 'My Blog', icon:'pi pi-book'},
+      { label: 'Contact', icon:'pi pi-phone'}
+    ];
+
+  }
 }
